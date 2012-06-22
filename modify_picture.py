7f8e4640884f027fb82fb334fd2e61bde1388db7
Reid Baker <hamiltonbaker@yahoo.com>
2012-06-22 18:32:42 +0200

    Added doc strings

--- modify_picture.py
+++ modify_picture.py
@@ -1,8 +1,10 @@
+"""Example of how to use the Python Imaging Library"""
 import os
 import sys
 from PIL import Image
 
 def main():
+    """Retrives file entered, manipulates it and saves a copy with a new name"""
     full_filename =  sys.argv[1]
     if full_filename != None:
         try:
@@ -15,10 +17,23 @@ def main():
         print 'Must enter photo in this directory'
 
 def rotate_image(image_data):
+    """
+    Rotates an image
+    Inputs:
+        PIL Image
+    Returns:
+        Modified image data
+    """
     print 'rotating'
     return image_data
 
 def safe_save(full_filename, new_image_data):
+    """
+    Saves image with new name
+    Inputs:
+        full_filename, filename to be saved including extention e.g, myfile.jpg
+        new_image_data, PIL Image data to be saved
+    """
     filename, extention = os.path.splitext(full_filename)
     new_image_data.save(filename + '_modified' + extention)
 
