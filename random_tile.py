50b1a61febd830f8abac305fe92ccc8390b2fb77
Reid Baker <hamiltonbaker@yahoo.com>
2012-07-03 02:07:20 +0200

    working :)

--- random_tile.py
+++ random_tile.py
@@ -40,21 +40,22 @@ def main():
 def tileify(image_data, num_blocks):
     width = image_data.size[0]
     height = image_data.size[1]
-    block_size = round(height / num_blocks)
+    row_block_size = round(height / num_blocks)
+    col_block_size = round(width / num_blocks)
     negitive_image = ImageOps.invert(image_data)
     pixels = numpy.array(image_data)
     for row in xrange(num_blocks):
-        inner_row_val = int(block_size * row)
-        outer_row_val = int(block_size * row + block_size)
+        inner_row_val = int(row_block_size * row)
+        outer_row_val = int(row_block_size * row + row_block_size)
         for col in xrange(num_blocks):
-            inner_col_val = int(block_size * col)
-            outer_col_val = int(block_size * col + block_size)
+            inner_col_val = int(col_block_size * col)
+            outer_col_val = int(col_block_size * col + col_block_size)
             block = pixels[
                 inner_row_val:outer_row_val,
                 inner_col_val:outer_col_val
             ]
-            x_offset = numpy.random.random_integers(1,10)
-            y_offset = numpy.random.random_integers(1,10)
+            x_offset = numpy.random.random_integers(-10,10)
+            y_offset = numpy.random.random_integers(-10,10)
             location = (
                 inner_col_val + y_offset,
                 inner_row_val + x_offset
