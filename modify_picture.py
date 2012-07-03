1a12db84f4d740e308da67a4a7b21565636dbdfb
Reid Baker <hamiltonbaker@yahoo.com>
2012-07-03 12:53:42 +0200

    refactored filmify

--- modify_picture.py
+++ modify_picture.py
@@ -4,7 +4,7 @@ import optparse
 
 from PIL import Image
 from PIL import ImageDraw
-from PIL import ImageOps
+from image_transforms import filmify_image
 
 def main():
     """Retrieves file entered, manipulates it and saves a copy with a new name"""
@@ -44,66 +44,6 @@ def main():
     else:
         print 'Must enter photo in this directory'
 
-def filmify_image(image_data):
-    """
-    Makes the image look like 35mm film
-    Inputs:
-        PIL Image
-    Returns:
-        Modified image data
-    """
-    print 'Filming'
-    # Size of photo in side the film
-    film_size = (400, 300)
-    border_size = 30
-    film_hole_size = (8, 10)
-
-    filmed_image = modify_photo(image_data, film_size)
-    filmed_image = add_film_border(filmed_image, film_size, border_size)
-
-    # Center strip in the border space
-    strip_upper_offset = int(round((border_size - film_hole_size[1])/2))
-    strip_lower_offset = film_size[1] + border_size + strip_upper_offset
-    # Bring strip in towards the photo
-    offset = int(round(film_hole_size[1]/2))
-    strip_upper_offset = strip_upper_offset + offset
-    strip_lower_offset = strip_lower_offset - offset
-
-    place_film_strip(
-        filmed_image,
-        strip_upper_offset,
-        strip_lower_offset,
-        film_hole_size
-    )
-
-    return filmed_image
-
-def add_film_border(image_data, film_size, border_size):
-    """
-    Creates a black border around the image
-    Inputs
-        image_data - image to be manipulated
-        film_size - size of the internal picture
-        border_size - how wide you want the border on the top and bottom to be
-    Output
-        image with border
-    """
-    image_data = ImageOps.expand(
-        image_data,
-        border=border_size,
-        fill='black'
-    )
-    # Crop to cut half of border from right and left of image
-    crop_box = (
-        int(round(border_size/2)),
-        0,
-        film_size[0] + int(round(border_size * 1.5)),
-        film_size[1] + border_size * 2
-    )
-    image_data = image_data.crop(crop_box)
-    return image_data
-
-
 def add_text_header(image_data, text='Kodak'):
     """
     Add text to the top of the document
@@ -121,68 +61,6 @@ def add_text_header(image_data, text='Kodak'):
         text = 'Kodak'
     draw = ImageDraw.Draw(image_data)
     draw.text(text_location, text, fill=fill_color)
-
-def place_film_strip(image_data, y_offset_upper, y_offset_lower, film_hole_size):
-    """
-    Create strip of film_holes at y_offset
-    """
-    hole_distance = 15
-    left_film_buffer = 2
-    for x_offset in range(left_film_buffer, image_data.size[0], hole_distance):
-        place_film_hole(image_data, (x_offset, y_offset_upper), film_hole_size)
-        place_film_hole(image_data, (x_offset, y_offset_lower), film_hole_size)
-
-def place_film_hole(image_data, offset, film_hole_size):
-    """
-    Puts film hole at offset into the image provided
-    Inputs:
-        image_data - image file to have film hole pasted
-        offset - 2 tuple with x and y of the top left corner of film hole
-        film_hole_size - size of the rectangle to be made
-    Outputs
-        Original image with rectangle in location based on the offset
-    """
-    corner_radius = 2
-    film_hole_color = 'white'
-    film_hole = round_rectangle(film_hole_size, corner_radius, film_hole_color)
-    image_data.paste(film_hole, offset)
-
-def round_corner(radius, fill):
-    """
-    Draw a round corner
-    This code came from http://nadiana.com/pil-tutorial-basic-advanced-drawing
-    """
-    corner = Image.new('RGBA', (radius, radius), (0, 0, 0, 0))
-    draw = ImageDraw.Draw(corner)
-    draw.pieslice((0, 0, radius * 2, radius * 2), 180, 270, fill=fill)
-    return corner
-
-def round_rectangle(size, radius, fill):
-    """
-    Draw a rounded rectangle
-    This code came from http://nadiana.com/pil-tutorial-basic-advanced-drawing
-    """
-    width, height = size
-    rectangle = Image.new('RGBA', size, fill)
-    corner = round_corner(radius, fill)
-    rectangle.paste(corner, (0, 0))
-    # Rotate the corner and paste it
-    rectangle.paste(corner.rotate(90), (0, height - radius))
-    rectangle.paste(corner.rotate(180), (width - radius, height - radius))
-    rectangle.paste(corner.rotate(270), (width - radius, 0))
-    return rectangle
-
-def modify_photo(image_data, film_size):
-    """
-    Make the image grayscale and invert the colors
-    All manipulations for the original photo go here
-    """
-    modified_image = ImageOps.grayscale(image_data)
-    # Image.ANTIALIAS is best for down sizing
-    modified_image = modified_image.resize(film_size, Image.ANTIALIAS)
-    modified_image = ImageOps.invert(modified_image)
-    modified_image = ImageOps.flip(modified_image)
-    return modified_image
 
 def safe_save(full_filename, new_image_data):
     """
